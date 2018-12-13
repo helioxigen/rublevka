e1873d57b01f0849fee0766ac1dc4f1dfc414d75
Evgeny Rodionov <git@erodionov.ru>
2018-12-13 20:13:46 +0300

    chore: update api url

--- site-frontend/src/core/config/resources.js
+++ site-frontend/src/core/config/resources.js
@@ -1,16 +1,16 @@
 const envs = {
   production: {
-    API: process.env.API_ENDPOINT || '/api',
-    cloudfront: '//images.jqestate.ru',
+    API: process.env.API_ENDPOINT || "//api.jqestate.ru",
+    cloudfront: "//images.jqestate.ru"
   },
   development: {
-    API: process.env.API_ENDPOINT || '/api',
-    cloudfront: '//images.jqestate.ru',
+    API: process.env.API_ENDPOINT || "//api-dev.jqestate.ru",
+    cloudfront: "//images.jqestate.ru"
   },
   local: {
-    API: process.env.API_ENDPOINT || '/api-dev',
-    cloudfront: '//images.jqestate.ru',
-  },
+    API: process.env.API_ENDPOINT || "/api-dev",
+    cloudfront: "//images.jqestate.ru"
+  }
 };
 
 export const { API, cloudfront } = envs[process.env.APP_ENV];

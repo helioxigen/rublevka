18a902a70bd9d46edecdb8adf1ce1842329fe6fe
Evgeny Rodionov <git@erodionov.ru>
2019-02-12 08:59:09 +0300

    chore: update version & temporarily add no-cors

--- staff-frontend/src/jq-redux-api/api.js
+++ staff-frontend/src/jq-redux-api/api.js
@@ -15,7 +15,7 @@ const { apiPath } = envs[process.env.REACT_APP_ENV || 'development'];
 
 // TODO FIXME use queryparams
 export const get = (resource, qp) =>
-  fetch(apiPath + resource).then(res => res.json());
+  fetch(apiPath + resource, { mode: 'no-cors' }).then(res => res.json());
 
 export const post = () => {};
 

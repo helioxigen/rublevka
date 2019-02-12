32fae1259e5dd1fa88a09735bc2cc560b78ed4e2
Evgeny Rodionov <git@erodionov.ru>
2019-02-12 05:12:16 +0300

    fix: update api path

--- staff-frontend/src/jq-redux-api/api.js
+++ staff-frontend/src/jq-redux-api/api.js
@@ -1,16 +1,17 @@
 const envs = {
   production: {
-    apiPath: process.env.REACT_APP_API_ENDPOINT || 'https://api.jq.estate',
+    apiPath: process.env.REACT_APP_API_ENDPOINT || 'https://api.rublevka.ru',
   },
   development: {
-    apiPath: process.env.REACT_APP_API_ENDPOINT || 'https://api-dev.jq.estate',
+    apiPath:
+      process.env.REACT_APP_API_ENDPOINT || 'https://api-dev.rublevka.ru',
   },
   local: {
     apiPath: process.env.REACT_APP_API_ENDPOINT || '',
   },
 };
 
-const { apiPath } = envs[process.env.APP_ENV || 'local'];
+const { apiPath } = envs[process.env.REACT_APP_ENV || 'local'];
 
 // TODO FIXME use queryparams
 export const get = (resource, qp) =>

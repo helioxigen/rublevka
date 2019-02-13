3a1179ff959ca1b40855a357141c8b7e7a9533d6
Evgeny Rodionov <git@erodionov.ru>
2019-02-13 12:23:44 +0300

    fix: remove cors-proxy

--- export-app-firebase/index.js
+++ export-app-firebase/index.js
@@ -1,5 +1,3 @@
 const { handler } = require('./src');
-const { cors } = require('./src/cors-proxy');
 
 exports.handler = handler;
-exports.cors = cors;
--- staff-frontend/src/jq-redux-api/api.js
+++ staff-frontend/src/jq-redux-api/api.js
@@ -4,10 +4,7 @@ const envs = {
   },
   development: {
     apiPath:
-      process.env.REACT_APP_API_ENDPOINT ||
-      `https://us-central1-rublevka-export-384da.cloudfunctions.net/cors?url=${encodeURI(
-        'https://dev.rublevka.ru/api',
-      )}`,
+      process.env.REACT_APP_API_ENDPOINT || 'https://dev.rublevka.ru/api',
   },
   local: {
     apiPath: process.env.REACT_APP_API_ENDPOINT || '',

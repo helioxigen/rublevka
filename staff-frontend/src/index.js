02c496f3aa93911ecd86b16b113a4fd48332774c
Evgeny Rodionov <github@erodionov.ru>
2019-03-26 14:20:18 +0300

    Feat: Add YM and Clean Up Sentry @ staff (#129)

--- staff-frontend/src/index.js
+++ staff-frontend/src/index.js
@@ -4,19 +4,13 @@ import * as Sentry from '@sentry/browser';
 import App from './App';
 import 'normalize.css';
 import './index.css';
-// import * as serviceWorker from './serviceWorker';
 
-if (
-  process.env.NODE_ENV === 'production' &&
-  process.env.REACT_APP_DRONE_COMMIT
-) {
+if (process.env.REACT_APP_ENV === 'production') {
   Sentry.init({
-    dsn: process.env.REACT_APP_STAFF_SENTRY_DSN,
+    dsn: process.env.REACT_APP_SENTRY_DSN,
     environment: process.env.REACT_APP_ENV,
-    release: process.env.DRONE_COMMIT,
+    // release: process.env.DRONE_COMMIT,
   });
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// serviceWorker.unregister();

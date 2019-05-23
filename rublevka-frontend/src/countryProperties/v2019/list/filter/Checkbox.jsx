83a09b9c410ac6a5ca151dc8dd16927a52aef340
helioxigen <alexanderzatulyviter@gmail.com>
2019-05-23 23:45:06 +0300

    "Only" button for checkboxes & hover

--- rublevka-frontend/src/countryProperties/v2019/list/filter/Checkbox.jsx
+++ rublevka-frontend/src/countryProperties/v2019/list/filter/Checkbox.jsx
@@ -2,11 +2,55 @@ import React from 'react';
 
 import UI from '../../../../ui/v2019';
 import { CheckboxLabel } from './styled';
+import styled from 'styled-components';
 
 const { Checkbox } = UI;
 
-export default ({ label, checked, onChange, name }) => (
-  <Checkbox checked={checked} handleChange={() => onChange(name, checked)}>
+const FilterCheckbox = ({
+  className,
+  label,
+  checked,
+  onChange,
+  onOnlyClick,
+  name,
+}) => (
+  <Checkbox
+    className={className}
+    checked={checked}
+    handleChange={() => onChange(name, checked)}
+  >
     <CheckboxLabel>{label}</CheckboxLabel>
+    {onOnlyClick && (
+      <a tabIndex={0} onClick={onOnlyClick(name)} role="button">
+        Только
+      </a>
+    )}
   </Checkbox>
 );
+
+export default styled(FilterCheckbox)`
+  padding: 6px;
+  transition: background-color 0.2s;
+
+  a {
+    color: #f44336;
+    display: none;
+
+    &:hover {
+      text-decoration: underline;
+    }
+  }
+
+  .label {
+    justify-content: space-between;
+  }
+
+  &:hover {
+    background: #f2f2f2;
+    border-radius: 2px;
+
+    a {
+      display: block;
+    }
+  }
+`;

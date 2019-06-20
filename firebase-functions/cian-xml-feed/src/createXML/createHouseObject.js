f8584f7305efea20172b439eb61a876a54725afc
Pterko <pter96@gmail.com>
2019-06-20 14:29:50 +0300

    [Cian XML Feed] Changed agent and client fees and bonuses to 0

--- firebase-functions/cian-xml-feed/src/createXML/createHouseObject.js
+++ firebase-functions/cian-xml-feed/src/createXML/createHouseObject.js
@@ -121,10 +121,10 @@ exports.createHouseObject = item => {
           BargainTerms: [
             { Price: item[`${item.offerKind}Offer`].price },
             { Currency: item[`${item.offerKind}Offer`].currency.toLowerCase() },
-            { ClientFee: 50 },
-            { AgentFee: 50 },
+            { ClientFee: 0 },
+            { AgentFee: 0 },
             {
-              AgentBonus: [{ Value: 50 }, { PaymentType: 'percent' }],
+              AgentBonus: [{ Value: 0 }, { PaymentType: 'percent' }],
             },
           ],
         },

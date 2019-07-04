6d38606243166770596db71cc82b5f84e0e4b6c1
helioxigen <alexanderzatulyviter@gmail.com>
2019-07-04 16:02:43 +0300

    [Next] Pagination logic

--- rublevka-frontend-next/components/Catalog/Pagination/index.jsx
+++ rublevka-frontend-next/components/Catalog/Pagination/index.jsx
@@ -5,8 +5,19 @@ import { Button, Icon } from '@components/UI';
 import PageLink from './PageLink';
 
 const Pagination = ({ className, count, currentPage }) => {
-    const topBorder = count - currentPage >= 3 ? currentPage : currentPage - 2;
-    const paginationEnd = count - currentPage < 3 ? count - 2 : currentPage;
+    const isLeft = currentPage < 3;
+    const isRight = count - currentPage < 3;
+
+    const pad = {
+        start: currentPage - 1,
+        end: currentPage + 2,
+    };
+
+    const rightStart = isRight ? count - 3 : pad.start;
+    const rightEnd = isRight ? count : pad.end;
+
+    const start = isLeft ? 2 : rightStart;
+    const end = isLeft ? 5 : rightEnd;
 
     return (
         <div className={className}>
@@ -15,7 +26,7 @@ const Pagination = ({ className, count, currentPage }) => {
                 <Icon name="arrow-left" viewBox="0 0 6 10" />
                 <PageLink page={1} current={currentPage === 1} />
                 {currentPage > 3 && <span className="eclipse">...</span>}
-                {range((currentPage < 3 ? 3 : topBorder) - 1, paginationEnd + 2).map(num => (
+                {range(start, end).map(num => (
                     <PageLink key={num} page={num} current={currentPage === num} />
                 ))}
                 {count > 4 && count - currentPage > 2 && <span className="eclipse">...</span>}
@@ -34,6 +45,10 @@ export default styled(Pagination)`
     .eclipse {
         width: 2.5em;
         text-align: center;
+    }
+
+    ${Button} {
+        margin-bottom: 32px;
     }
 
     .pages {

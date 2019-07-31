edf246624644b721dc92eae6cbfdd5e994e8c561
helioxigen <alexanderzatulyviter@gmail.com>
2019-07-31 19:22:50 +0300

    [Next/Fetch] Fixed empty response

--- rublevka-frontend/api/instance.js
+++ rublevka-frontend/api/instance.js
@@ -6,6 +6,7 @@ require('isomorphic-fetch');
 export const instance = (apiPath, params) =>
     fetch(`${config.resource.API_ENDPOINT}/${apiPath}${params ? `?${encodeURI(query.get(params))}` : ''}`, {
         method: 'GET',
+        cache: 'no-cache',
     }).then(r => r.json());
 
 // export const getCountryProperties = (pagination, filter) => instance('properties/country', { pagination, filter });

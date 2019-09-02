86e7f74a687e178f436aecef63fefc338ebde314
helioxigen <alexanderzatulyviter@gmail.com>
2019-09-02 04:13:17 +0300

    [CDN] create uploadcare image link

--- rublevka-frontend/utils/cdn.js
+++ rublevka-frontend/utils/cdn.js
@@ -1,7 +1,13 @@
 import { CDN } from '@config/resource';
 import app from './app';
 
-const getImageLink = (id, postfix = app.config.name, size = 1024) => `${CDN}/${id}-${postfix}-${size}`;
+const getImageLink = (id, postfix = app.config.name, size = 1024) => {
+    if (id.startsWith('CLOUD:')) {
+        return `https://ucarecdn.com/${id.replace('CLOUD:', '')}/-/resize/${size}/image.jpg`;
+    }
+
+    return `${CDN}/${id}-${postfix}-${size}`;
+};
 
 export default {
     get: {

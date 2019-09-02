c98938ead9fc00f603e0ebb4fcedf4917be01a48
helioxigen <alexanderzatulyviter@gmail.com>
2019-09-02 04:29:51 +0300

    [CDN] Use watermark

--- rublevka-frontend/utils/cdn.js
+++ rublevka-frontend/utils/cdn.js
@@ -3,7 +3,9 @@ import app from './app';
 
 const getImageLink = (id, postfix = app.config.name, size = 1024) => {
     if (id.startsWith('CLOUD:')) {
-        return `https://ucarecdn.com/${id.replace('CLOUD:', '')}/-/resize/${size}/image.jpg`;
+        const watermark = size > 512 ? '/-/overlay/7bdb5192-4a14-4cca-bda9-3ab59670f78f/30%25x30%25/center' : '';
+
+        return `https://ucarecdn.com/${id.replace('CLOUD:', '')}${watermark}/-/resize/${size}/image.jpg`;
     }
 
     return `${CDN}/${id}-${postfix}-${size}`;
